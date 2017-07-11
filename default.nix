3ca093af96ef38fc64d039195afb4aabaeaa7656
patternspandemic <patternspandemic@live.com>
2017-07-11 14:39:12 -0700

    Add default.nix, korefile.js, ignore and move some things around.

---
+++ default.nix
@@ -0,0 +1,29 @@
+{ stdenv, lib, alsaLib, mesa, xlibs, xorg, kore}:
+
+stdenv.mkDerivation rec {
+  name = "libKoreC";
+  src = ./.;
+
+  buildInputs = [];
+
+  # sourceRoot = ".";
+
+  installPhase = ''
+    mkdir -p $out/lib
+    mkdir -p $out/include
+  '';
+
+  # postFixup = lib.optionalString (stdenv.system == "i686-linux" || stdenv.system == "x86_64-linux") ''
+  #   substituteInPlace $out/resources/app/extensions/kha/Kha/Tools/khamake/node_modules/fs-extra/lib/copy-sync/copy-file-sync.js --replace "stat.mode" "33188"
+  #   substituteInPlace $out/resources/app/extensions/kha/Kha/Kore/Tools/koremake/node_modules/fs-extra/lib/copy-sync/copy-file-sync.js --replace "stat.mode" "33188"
+  # '';
+
+  meta = with stdenv.lib; {
+    description = "Shared library wrapping Kode's Kore framework";
+    homepage = https://github.com/patternspandemic/KoreC;
+    downloadPage = https://github.com/patternspandemic/KoreC;
+    license = licenses.zlib;
+    maintainers = [ maintainers.patternspandemic ];
+    platforms = [ "x86_64-linux" ];
+  };
+}
---
+++ korefile.js
@@ -0,0 +1,9 @@
+let project = new Project('KoreC', __dirname);
+
+project.addFile('Sources/**');
+project.setDebugDir('Deployment');
+
+Project.createProject('Kore', __dirname).then((subproject) => {
+	project.addSubProject(subproject);
+	resolve(project);
+});
